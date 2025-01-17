82e0b9eac6206bb4535894a0fe05473eacd43b25
twinh <twinhuang@qq.com>
2025-01-17 16:43:33 +0800

    refactor(internal): 生成元数据到模型类中，删除 Trait

--- src/Service/SmsTemplateModel.php
+++ src/Service/SmsTemplateModel.php
@@ -6,14 +6,25 @@ use Miaoxing\Plugin\BaseModel;
 use Miaoxing\Plugin\Model\ModelTrait;
 use Miaoxing\Plugin\Model\ReqQueryTrait;
 use Miaoxing\Plugin\Model\SnowflakeTrait;
-use Miaoxing\Sms\Metadata\SmsTemplateTrait;
 use Wei\Model\SoftDeleteTrait;
 
+/**
+ * @property string|null $id
+ * @property string $name 名称
+ * @property string $code 标识
+ * @property string $templateId 模板编号
+ * @property string $remark 备注
+ * @property string|null $createdAt
+ * @property string|null $updatedAt
+ * @property string $createdBy
+ * @property string $updatedBy
+ * @property string|null $deletedAt
+ * @property string $deletedBy
+ */
 class SmsTemplateModel extends BaseModel
 {
     use ModelTrait;
     use ReqQueryTrait;
-    use SmsTemplateTrait;
     use SnowflakeTrait;
     use SoftDeleteTrait;
 }
